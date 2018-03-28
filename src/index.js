ca820e7e69fd4fe1221f5f8341c773fb54d47410
SAWADA Takayoshi <sawadasuiren@gmail.com>
2018-03-28 22:59:58 +0900

    Remove flow

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-// @flow
+import React from 'react';
+import { injectGlobal } from 'styled-components';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './app/store';
+import App from './app/App';
+
+injectGlobal`
+  html,
+  body {
+    background: black;
+    color: white;
+  }
+
+  #Main {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const renderApp = () => {
+  const target = document.getElementById('Main');
+  if (target instanceof HTMLElement) {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      target,
+    );
+  }
+};
+renderApp();
